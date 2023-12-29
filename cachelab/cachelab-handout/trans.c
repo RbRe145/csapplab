29d44b46f4f7881e623d39e7b7ea26fcf0f1fb42
RbRe145 <czheng12399@outlook.com>
2023-12-29 14:45:07 +0800

    64X64 bug unfinish

--- cachelab/cachelab-handout/trans.c
+++ cachelab/cachelab-handout/trans.c
@@ -9,6 +9,7 @@
  */ 
 #include <stdio.h>
 #include "cachelab.h"
+#define bsize 8
 
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 
@@ -19,9 +20,220 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
  *     searches for that string to identify the transpose function to
  *     be graded. 
  */
+
+char transpose_test_1[] = "test 4 64X64 with tile 4X4 to solve mapping conflict";
+void transpose_4regs(int M, int N, int A[N][M], int B[M][N])
+{
+    int i, j;
+    int tmp0, tmp1, tmp2, tmp3;
+
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < M; j+=4) {
+            tmp0 = A[i][j];
+            tmp1 = A[i][j+1];
+            tmp2 = A[i][j+2];
+            tmp3 = A[i][j+3];
+
+            B[j][i] = tmp0;
+            B[j+1][i] = tmp1;
+            B[j+2][i] = tmp2;
+            B[j+3][i] = tmp3;
+        }
+    }    
+}
+
+char transpose_test_4[] = "test 4 for 64*64";
+void transpose_prefetch(int M, int N, int A[N][M], int B[M][N])
+{
+    int i, j, tile_i;
+    int tmp0, tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7;
+
+    for (i = 0; i < N; i+=8) {
+        for (j = 0; j < M; j+=8) {
+            tmp0 = A[i+0][j];
+            tmp1 = A[i+1][j];
+            tmp2 = A[i+2][j];
+            tmp3 = A[i+3][j];
+            tmp4 = A[i+4][j];
+            tmp5 = A[i+5][j];
+            tmp6 = A[i+6][j];
+            tmp7 = A[i+7][j];
+            for(tile_i=0; tile_i<8; tile_i++){
+                tmp0 = A[i+tile_i][j];
+                tmp1 = A[i+tile_i][j+1];
+                tmp2 = A[i+tile_i][j+2];
+                tmp3 = A[i+tile_i][j+3];
+                tmp4 = A[i+tile_i][j+4];
+                tmp5 = A[i+tile_i][j+5];
+                tmp6 = A[i+tile_i][j+6];
+                tmp7 = A[i+tile_i][j+7];
+
+                B[j][i+tile_i] = tmp0;
+                B[j+1][i+tile_i] = tmp1;
+                B[j+2][i+tile_i] = tmp2;
+                B[j+3][i+tile_i] = tmp3;
+                B[j+4][i+tile_i] = tmp4;
+                B[j+5][i+tile_i] = tmp5;
+                B[j+6][i+tile_i] = tmp6;
+                B[j+7][i+tile_i] = tmp7;
+            }
+            tile_i = 0;
+        }
+    }    
+}
+
+char transpose_test_3[] = "32X32 solution";
+void transpose_8X8tile_32X32(int M, int N, int A[N][M], int B[M][N])
+{
+    int i, j, tile_i;
+    int tmp0, tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7;
+
+    for (j = 0; j < N; j+=8) {
+        for (i = 0; i < M; i+=8) {
+            for(tile_i=0; tile_i<8; tile_i++){
+                tmp0 = A[i+tile_i][j];
+                tmp1 = A[i+tile_i][j+1];
+                tmp2 = A[i+tile_i][j+2];
+                tmp3 = A[i+tile_i][j+3];
+                tmp4 = A[i+tile_i][j+4];
+                tmp5 = A[i+tile_i][j+5];
+                tmp6 = A[i+tile_i][j+6];
+                tmp7 = A[i+tile_i][j+7];
+
+                B[j][i+tile_i] = tmp0;
+                B[j+1][i+tile_i] = tmp1;
+                B[j+2][i+tile_i] = tmp2;
+                B[j+3][i+tile_i] = tmp3;
+                B[j+4][i+tile_i] = tmp4;
+                B[j+5][i+tile_i] = tmp5;
+                B[j+6][i+tile_i] = tmp6;
+                B[j+7][i+tile_i] = tmp7;
+            }
+            tile_i = 0;
+        }
+    }    
+}
+
+char transpose_test_5[] = "test 5 solution for 64*64 using 4*4tile";
+void transpose_4X4tile_64X64(int M, int N, int A[N][M], int B[M][N])
+{
+    int i, j, tile_i;
+    int tmp0, tmp1, tmp2, tmp3;
+
+    for (j = 0; j < N; j+=4) {
+        for (i = 0; i < M; i+=4) {
+            for(tile_i=0; tile_i<4; tile_i++){
+                tmp0 = A[i+tile_i][j];
+                tmp1 = A[i+tile_i][j+1];
+                tmp2 = A[i+tile_i][j+2];
+                tmp3 = A[i+tile_i][j+3];
+
+                B[j][i+tile_i] = tmp0;
+                B[j+1][i+tile_i] = tmp1;
+                B[j+2][i+tile_i] = tmp2;
+                B[j+3][i+tile_i] = tmp3;
+            }
+            tile_i = 0;
+        }
+    }    
+}
+
+char transpose_test_7[] = "64X64 solution with magic";
+void transpose_8X8tile_64X64(int M, int N, int A[N][M], int B[M][N])
+{
+    int i, j, tile_i;
+    int tmp0, tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7;
+
+    for (j = 0; j < N; j+=8) {
+        for (i = 0; i < M; i+=8) {
+            for(tile_i=0; tile_i<4; tile_i++){
+                tmp0 = A[i+tile_i][j];
+                tmp1 = A[i+tile_i][j+1];
+                tmp2 = A[i+tile_i][j+2];
+                tmp3 = A[i+tile_i][j+3];
+                tmp4 = A[i+tile_i][j+4];
+                tmp5 = A[i+tile_i][j+5];
+                tmp6 = A[i+tile_i][j+6];
+                tmp7 = A[i+tile_i][j+7];
+
+                B[j][i+tile_i] = tmp0;
+                B[j+1][i+tile_i] = tmp1;
+                B[j+2][i+tile_i] = tmp2;
+                B[j+3][i+tile_i] = tmp3;
+                B[j+0][i+tile_i+4] = tmp4;
+                B[j+1][i+tile_i+4] = tmp5;
+                B[j+2][i+tile_i+4] = tmp6;
+                B[j+3][i+tile_i+4] = tmp7;
+            }
+            tile_i = 0;
+            for(tile_i=0; tile_i<4; tile_i++){
+                tmp0 = A[i+tile_i+4][j];
+                tmp1 = A[i+tile_i+4][j+1];
+                tmp2 = A[i+tile_i+4][j+2];
+                tmp3 = A[i+tile_i+4][j+3];
+                tmp4 = B[j+0][i+tile_i+4];
+                tmp5 = B[j+1][i+tile_i+4];
+                tmp6 = B[j+2][i+tile_i+4];
+                tmp7 = B[j+3][i+tile_i+4];
+
+                B[j+0][i+4+tile_i] = tmp0;
+                B[j+1][i+4+tile_i] = tmp1;
+                B[j+2][i+4+tile_i] = tmp2;
+                B[j+3][i+4+tile_i] = tmp3;
+                B[j+4+tile_i][i+0] = tmp4;
+                B[j+4+tile_i][i+1] = tmp5;
+                B[j+4+tile_i][i+2] = tmp6;
+                B[j+4+tile_i][i+3] = tmp7;
+            }
+            tile_i = 0;
+            for(tile_i=0; tile_i<4; tile_i++){
+                tmp0 = A[i+4+tile_i][j+4+0];
+                tmp1 = A[i+4+tile_i][j+4+1];
+                tmp2 = A[i+4+tile_i][j+4+2];
+                tmp3 = A[i+4+tile_i][j+4+3];
+               
+                B[j+4+0][i+4+tile_i] = tmp0;
+                B[j+4+1][i+4+tile_i] = tmp1;
+                B[j+4+2][i+4+tile_i] = tmp2;
+                B[j+4+3][i+4+tile_i] = tmp3;
+
+            }
+            tile_i = 0;
+        }
+    }    
+}
+
+
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+    int i, j, tile_i;
+    int tmp0, tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7;
+
+    for (i = 0; i < N; i+=8) {
+        for (j = 0; j < M; j+=8) {
+            for(tile_i=0; tile_i<8; tile_i++){
+                tmp0 = A[i+tile_i][j];
+                tmp1 = A[i+tile_i][j+1];
+                tmp2 = A[i+tile_i][j+2];
+                tmp3 = A[i+tile_i][j+3];
+                tmp4 = A[i+tile_i][j+4];
+                tmp5 = A[i+tile_i][j+5];
+                tmp6 = A[i+tile_i][j+6];
+                tmp7 = A[i+tile_i][j+7];
+
+                B[j][i+tile_i] = tmp0;
+                B[j+1][i+tile_i] = tmp1;
+                B[j+2][i+tile_i] = tmp2;
+                B[j+3][i+tile_i] = tmp3;
+                B[j+4][i+tile_i] = tmp4;
+                B[j+5][i+tile_i] = tmp5;
+                B[j+6][i+tile_i] = tmp6;
+                B[j+7][i+tile_i] = tmp7;
+            }
+            tile_i = 0;
+        }
+    }    
 }
 
 /* 
@@ -56,10 +268,14 @@ void trans(int M, int N, int A[N][M], int B[M][N])
 void registerFunctions()
 {
     /* Register your solution function */
+    // 可以在这里注册很多个函数，只要记得住名称
+    // registerTransFunction(transpose_4X4tile, transpose_test_2); 
+    registerTransFunction(transpose_8X8tile_64X64, transpose_test_7); 
+
     registerTransFunction(transpose_submit, transpose_submit_desc); 
 
     /* Register any additional transpose functions */
-    registerTransFunction(trans, trans_desc); 
+    // registerTransFunction(trans, trans_desc); 
 
 }
 
